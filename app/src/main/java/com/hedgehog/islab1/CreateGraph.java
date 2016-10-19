59837498e308227cf7b031c7a7a0d42a3c5290e4
DenisHedgehog <ezhoden@mail.ru>
2016-10-19 17:11:17 +0300

    Almost done.

--- app/src/main/java/com/hedgehog/islab1/CreateGraph.java
+++ app/src/main/java/com/hedgehog/islab1/CreateGraph.java
@@ -1,11 +1,8 @@
 package com.hedgehog.islab1;
 
-import android.app.Application;
-import android.content.Context;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 /**
  * Created by hedgehog on 15.10.16.
@@ -13,20 +10,13 @@ import java.util.Arrays;
 
 public class CreateGraph {
 
-    //Создаем вершину и добавляем её в массив вершин
-
-    private Graph graph;
-
 
     public ArrayList<Vertex> getVertices() {
         return vertices;
     }
 
     public void addVertex(int number, Vertex vertex) {
-
         getVertices().add(number, vertex);
-        //String g = Arrays.toString(getVertices().get(number).getAdjacentVertices());
-
     }
 
     private ArrayList<Vertex> vertices;
@@ -42,18 +32,5 @@ public class CreateGraph {
         }
 
     }
-
-    /*public String[] numbersToString() {
-        String[] strings = new String[];
-        for (int i = 0; i < getVertices().size(); i++) {
-            strings[i] = getVertices().get(i).toString();
-        }
-        return strings;
-    }*/
-
-    /*@Override
-    public String toString() {
-        return getClass().getField()
-    }*/
 
 }
